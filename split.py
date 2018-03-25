dba46df4056d4b85b92cd432219851ab0c5e4d9d
Andrew Bitterling <andrew@drewbitt.com>
2018-03-25 15:45:36 -0400

    psuedocode without datetime implementation

--- split.py
+++ split.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 
 def split_file(file):
     with open(file) as f:
@@ -14,12 +15,29 @@ def split_file(file):
         if count== len(split)-1:
             break
 
+        initial_time = 0
+
         with open('chapters' + str(count+1) + '.txt', 'w') as output_file:
             # loop over sliced list of lines
-            for line in content[chapter_num*2:]:
+            for inner_count, line in enumerate(content[chapter_num*2:]):
                 count_lines = count_lines + 1
 
-                output_file.write(line)
+                # Adjust time on odd number lines
+                if count_lines % 2 == 0:
+                    output_file.write(line)
+                else:
+                    # split line into before and after =
+                    # time is now line_time[1]
+                    line_time = line.split("=")
+
+                    if inner_count == 1:
+                        # first time line = initial time to adjust by
+                        initial_time = line_time[1]
+                        time = "00:00:00:00"
+                    else:
+                        time = line_time[1] - initial_time
+                    output_file.write(line_time[0]+"="+time)
+
                 # if line num = chapter number of end, break
                 if count_lines == split[count+1] * 2:
                     break

e74cefb38682f61de70c7ec33ce599049017f8dd
Andrew Bitterling <andrew@drewbitt.com>
2018-03-25 16:38:39 -0400

    close, but timedelta only has one digit hour?

--- split.py
+++ split.py
@@ -15,28 +15,35 @@ def split_file(file):
         if count== len(split)-1:
             break
 
-        initial_time = 0
+        initial_time = datetime.time()
 
         with open('chapters' + str(count+1) + '.txt', 'w') as output_file:
             # loop over sliced list of lines
             for inner_count, line in enumerate(content[chapter_num*2:]):
                 count_lines = count_lines + 1
 
-                # Adjust time on odd number lines
-                if count_lines % 2 == 0:
-                    output_file.write(line)
-                else:
+                # Adjust time for every other line
+                if inner_count % 2 == 0:
                     # split line into before and after =
                     # time is now line_time[1]
                     line_time = line.split("=")
 
-                    if inner_count == 1:
+                    if inner_count == 0:
+                        ex_time = datetime.time()
                         # first time line = initial time to adjust by
                         initial_time = line_time[1]
-                        time = "00:00:00:00"
+                        initial_time = datetime.datetime.strptime(initial_time, "%H:%M:%S.%f")
                     else:
-                        time = line_time[1] - initial_time
-                    output_file.write(line_time[0]+"="+time)
+                        ex_time = line_time[1]
+                        ex_time = datetime.datetime.strptime(ex_time, "%H:%M:%S.%f")
+                        ex_time = ex_time - initial_time
+                    if isinstance(ex_time, datetime.time):
+                        print(ex_time.strftime("%H:%M:%S.%f")[:-3])
+                    else:
+                        print(str(ex_time)[:-3])
+                    # output_file.write(line_time[0]+"="+ex_time)
+                else:
+                    output_file.write(line)
 
                 # if line num = chapter number of end, break
                 if count_lines == split[count+1] * 2:

af0898997899dbedb0953435ec697d549324d310
Andrew <andrew@drewbitt.com>
2018-08-15 06:43:22 -0500

    Can change generated filename format now

--- README.md
+++ README.md
@@ -6,9 +6,12 @@ Useful when multiple episodes are combined in one mpls.
 
 
 Usage:
-`split.py filename.txt --indexes 0,4,9,15,18 --offset 1 --titles`
+`split.py filename.txt --indexes 0,4,9,15,18 --offset 1 --file-name "chapters$n" --titles`
 
 The example would have 0-4,5-9,10-15,16-18 together. Need to include the first and last chapter.
+
 `--offset/-o` determines the starting index number for the generated files and is a default of 1. `--titles` is optionally included if you want generic "Chapter 01, Chapter 02" chapter names.
+
+`--file-name` defaults to `chapters%n`. Use it to specify the name format for generated files, `%n` is used to specify the split number location in the string.
 
 You can also use `--only-titles` for only creating generic chapter title names without creating additional chapter files.
--- split.py
+++ split.py
@@ -6,13 +6,17 @@ from os import fdopen, remove
 from shutil import move
 
 
-def split_file(file, offset, names, only_names):
-    """Main method to split the chapter file"""
+def split_file(file, names, only_names, rsplit, offset=1, file_name_format="chapters%n"):
+    """Main method to split the chapter file
+        file: main chapter file, names: boolean for if to add generic chapter names,
+        only_names: boolean to ONLY add generic chapter names without creating a new file,
+        rsplit: array of indexes to include in each file (see README for split explanation),
+        offset: what to start numbering at for filenames, file_name_format: format for new filenames"""
 
     if only_names:
-        #Create temp file
+        # Create temp file
         fh, abs_path = mkstemp()
-        with fdopen(fh,'w') as new_file:
+        with fdopen(fh, 'w') as new_file:
             with open(file, 'r') as old_file:
                 for count, line in enumerate(old_file):
                     if count % 2 != 0:
@@ -31,13 +35,14 @@ def split_file(file, offset, names, only_names):
         # keeps track of how many lines are processed in total in loop
         count_lines = 0
 
-        for count, chapter_num in enumerate(split):
-            if count == len(split) - 1:
+        for count, chapter_num in enumerate(rsplit):
+            if count == len(rsplit) - 1:
                 break
 
             initial_time = datetime.time()
 
-            with open('chapters' + str(count + offset) + '.txt', 'w') as output_file:
+            file_name = file_name_format.replace("%n", str(count + offset).zfill(2))
+            with open(file_name + '.txt', 'w') as output_file:
                 # loop over sliced list of lines
                 for inner_count, line in enumerate(content[chapter_num * 2:]):
                     count_lines = count_lines + 1
@@ -95,7 +100,7 @@ def split_file(file, offset, names, only_names):
                             output_file.write("Chapter " + chap_num)
 
                     # if line num = chapter number of end specified in split, break
-                    if count_lines == split[count + 1] * 2:
+                    if count_lines == rsplit[count + 1] * 2:
                         break
                     output_file.write("\n")
 
@@ -105,6 +110,8 @@ if __name__ == "__main__":
     parser.add_argument('file', nargs=1)
     parser.add_argument('--indexes', '-i', help="Indexes to split the files at")
     parser.add_argument('--offset', '-o', type=int, default=1, help="Number to start file numbering at. Default 1")
+    parser.add_argument('--file-name', '-f', dest="file_name", default="chapters%n",
+                        help="Name format for generated files. Use %n to specify number location. Defaults to chapters%n")
     parser.add_argument('--titles', action="store_true", help="Set if you want to set generic chapter titles.")
     parser.add_argument('--only-titles', dest="only_titles", action="store_true",
                         help="Set for only doing generic chapter titles. Ignores all other arguments and edits the same file as given.")
@@ -113,4 +120,4 @@ if __name__ == "__main__":
     if not args.only_titles:
         split = [int(n) for n in args.indexes.split(',')]
     # don't really gotta pass any of these with this scope
-    split_file(args.file[0], args.offset, args.titles, args.only_titles)
+    split_file(args.file[0], args.titles, args.only_titles, split, args.offset, args.file_name)

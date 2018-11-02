3b81b3c0b0c66628b6825bdd7c82be20d45e6403
Andrew <andrew@drewbitt.com>
2018-11-02 03:53:19 -0400

    Working with one split

--- split_video.py
+++ split_video.py
@@ -22,7 +22,7 @@ def split_by_video(file, names, videos, offset=1, file_name_format="chapters%n")
 
 
 def getLength(filename):
-    """Returns length of file to the milisecond using ffprobe"""
+    """Returns length of file to the microsecond using ffprobe"""
     return FFProbe(filename).streams[0].duration
 
 
@@ -30,7 +30,7 @@ def convert_times(times):
     """ Convert times from ffprobe so they're usable by datetime """
     new_times = []
     for t in times:
-        # Doing manual math to convert seconds above 60 to minutes and seconds. Seems like there should be an easy function for this
+        # Doing manual math to convert seconds above 60 to minutes and seconds. Probably could've been done with timedeltas
         big_seconds = t.split(".")
         mth = int(big_seconds[0]) / 60
         frac, whole = math.modf(mth)
@@ -39,32 +39,56 @@ def convert_times(times):
         new_times.append(str(int(whole)) + "." + str(int(frac*60)) + "." + big_seconds[1])
     return new_times
 
+
 def parse_file(file, times):
     """ Read chapter file and find the time for each episode in successive order. Returns index to split at"""
 
-    total_time = datetime.time()
+    total_time = datetime.timedelta(0,0)
+    indexes = []
     time_arr_index = 0
+    first = None
 
     times = convert_times(times)
     # convert times array to datetime objects
     times = [datetime.datetime.strptime(t, "%M.%S.%f") for t in times]
 
     with open(file) as f:
-            content = f.readlines()
+        # Remove blank lines since I work with len(content)
+        content = [line for line in f.readlines() if line.strip()]
 
     # strip \n chars
     content = [x.strip() for x in content]
 
     for count, line in enumerate(content):
-        # Get time and compare to time in times[]
+        if first == None:
+            if count == 0:
+                first = 0
+            else:
+                first = math.ceil((count+1)/2)
 
+        # Get time and compare to time in times[]
         if count % 2 == 0:
             # time is now line_time[1]
             line_time = line.split("=")
 
             dt_line = datetime.datetime.strptime(line_time[1], "%H:%M:%S.%f")
 
-            # Compare time to time in times[time_arr_index]
+            # Compare time to time in times[time_arr_index] - within two seconds. Duration may need to change.
+            if dt_line - datetime.timedelta(0, 2) <= times[time_arr_index] + total_time <= dt_line + datetime.timedelta(0, 2):
+                total_time += times[time_arr_index]
+                time_arr_index += 1
+
+                indexes.extend([first, math.ceil((count+1)/2)])
+                first = None
+
+            if time_arr_index+1 > len(times):
+                # we done, add indexes for whats remaining
+                if count+2 < len(content):
+                    indexes.extend([math.ceil((count+1)/2)+1, int(len(content)/2)])
+                    break
+
+    print(indexes)
+    return indexes
 
 
 if __name__ == "__main__":
